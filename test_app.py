4014522727e6988acc013a08b44ba37541ecf6b3
Grace KOFFI <grvcek@GrvceLap.localdomain>
2026-05-28 16:05:24 +0200

    ajout initial tests

---
+++ test_app.py
@@ -0,0 +1,5 @@
+import sys
+import pytest
+
+if __name__ == "main":
+    sys.exit(pytest.main( ["-v", "tests"]))
---
+++ tests/test_backend.py
@@ -0,0 +1,2 @@
+from backend import init_tasks, add_task, get_tasks, mark_task_done, delete_task
+import sys
